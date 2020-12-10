970cab049677ccd2fdfe6dc296d1b93c4e8205c6
a-zcf <1312423338@qq.com>
2020-12-10 11:18:10 +0800

    配置生产环境和测试环境

---
+++ config/test.env.js
@@ -0,0 +1,5 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"testing"',
+  API_ROOT: '""' // 测试环境地址
+}
--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,4 @@
 const urls = {
-  base: '',
-  origin: '',
-
   // userInfo: '/api/index/userinfo', // 用户信息接口
 }
 module.exports = urls
